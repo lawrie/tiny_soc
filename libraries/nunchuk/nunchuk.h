63593c1cbc35664df28b4748f9183dc6390507d8
lawrie <lawrie.griffiths@ntlworld.com>
2018-09-20 10:52:06 +0100

    Added nunchuck.c

--- libraries/nunchuk/nunchuk.h
+++ libraries/nunchuk/nunchuk.h
@@ -1,45 +1,14 @@
 #ifndef __NUNCHUK_H__
 #define __NUNCHUK_H__
 
-#define reg_i2c_write (*(volatile uint32_t*)0x07000000)
-#define reg_i2c_read (*(volatile uint32_t*)0x07000004)
+#include <stdint.h>
 
-#define ADDRESS 0x52
+uint32_t i2c_get_status(void);
 
-uint32_t i2c_get_status() {
-  return reg_i2c_write;
-}
+void i2c_write(uint8_t r, uint8_t d);
 
-void i2c_write(uint8_t r, uint8_t d) {
-  reg_i2c_write = (ADDRESS << 24) | (r << 16) | (d << 8) | 0x80000000;
-}
- 
-void i2c_send_cmd(uint8_t r, uint8_t d) {
-  uint32_t status;
+void i2c_send_cmd(uint8_t r, uint8_t d);
 
-  i2c_write(r, d);
-
-  do {
-    status = i2c_get_status();
-  } while ((status >> 31) != 0);
-}
-
-void i2c_start_read() {
-  reg_i2c_read = (ADDRESS << 17) | 1;
-}
-
-uint8_t i2c_read() {
-  uint32_t status;
-
-  reg_i2c_read = (ADDRESS << 17) | 1;
-  for (uint32_t i = 0; i < 100; i++) asm volatile ("");
-	
-  do {
-    status = i2c_get_status();
-
-  } while((status >> 31) != 0);
- 
-  return status & 0xFF;
-}
+uint8_t i2c_read(void);
 
 #endif
